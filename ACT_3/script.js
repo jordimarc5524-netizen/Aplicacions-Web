121299eae31d7af67298b77f759d2ccd799c24f2
Jordi_Lumbreras <jordimarc5524@iessineu.net>
2026-06-09 19:42:55 +0200

    Java Script  & Local Storage end

---
+++ ACT_3/script.js
@@ -0,0 +1,46 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll(' .row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+let ticketPrice = movieSelect.value;
+
+// Save selected movie index and price
+function serMoivieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+localStorage
+
+//Update total and count
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+    const selectedSeatsCount = selectedSeats.length;
+
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+//movie select event
+movieSelect.addEventListener('change', (e) => {
+    ticketPrice = +e.target.value;
+    serMoivieData(e.target.selectedIndex, e.target.value);
+    updateSelectedCount();
+});
+
+
+//seat click event
+container.addEventListener('click', (e) => {
+    if (e.target.classList.contains('seat')&& 
+    !e.target.classList.contains('occupied')
+
+     ) {
+        e.target.classList.toggle('selected');
+        updateSelectedCount();
+    }
+ });
